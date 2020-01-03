5a372b9ca76e7e3c5c30797524dd8718c2655b56
Yann RENAUDIN <4748419+emyann@users.noreply.github.com>
2020-01-03 17:03:14 -0500

    Feat/add redux (#1)
    * feat: add base redux
    
    * feat: fetch messages
    
    * feat: dispatch add author on fetch messages
    
    * chore: cleanup
    
    * refactor: update slice name
    
    * refactor: update MesssagesState name
    
    * Yann le dozo
    
    Co-authored-by: Julien Stroheker <julienstroheker@gmail.com>

--- cmd/web/src/Questions.tsx
+++ cmd/web/src/Questions.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect,useState} from 'react';
+import React, { useEffect } from 'react';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -6,71 +6,51 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-
-import axios from 'axios';
-
-interface IMessage {
-    id: string;
-    author:  string;
-    message: string;
-    likes: number;
-}
-
-const API_URL = 'http://localhost:8080';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from './store/store';
+import { fetchMessages, getMessagesWithUser } from './store/messages';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       width: '100%',
-      backgroundColor: theme.palette.background.paper,
+      backgroundColor: theme.palette.background.paper
     },
     inline: {
-      display: 'inline',
-    },
-  }),
+      display: 'inline'
+    }
+  })
 );
 
 export default function AlignItemsList() {
   const classes = useStyles();
-
-  const [messages, setMessages] = useState<IMessage[]>([]);
-
+  const dispatch = useDispatch<AppDispatch>();
+  // Messages is updated everytime the store is updated
+  const messages = useSelector(getMessagesWithUser);
   useEffect(() => {
-    const url = `${API_URL}/messages`;
-    axios.get(url).then(response => response.data)
-    .then((data) => {
-        console.log("Data fetched: ", data)
-        setMessages(data)
-       })
-    }, [])
+    dispatch(fetchMessages());
+  }, [dispatch]);
 
   return (
     <List className={classes.root}>
-        {messages && messages.length > 0 ? (
-            messages.map(item => (
-                <ListItem alignItems="flex-start" key={item.id}>
-                <ListItemAvatar>
-                  <Avatar alt={item.author} src="/static/images/avatar/1.jpg" />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={item.message}
-                  secondary={
-                    <React.Fragment>
-                      <Typography
-                        component="span"
-                        variant="body2"
-                        className={classes.inline}
-                        color="textPrimary"
-                      >
-                      </Typography>
-                      {" — by "+item.author}
-                    </React.Fragment>
-                  }
-                />
-              </ListItem>
-            ))) : (
-                <React.Fragment></React.Fragment>
-            )}
+      {messages && messages.length > 0
+        ? messages.map(item => (
+            <ListItem alignItems="flex-start" key={item.message.id}>
+              <ListItemAvatar>
+                <Avatar alt={item.author.name} src="/static/images/avatar/1.jpg" />
+              </ListItemAvatar>
+              <ListItemText
+                primary={item.message.text}
+                secondary={
+                  <React.Fragment>
+                    <Typography component="span" variant="body2" className={classes.inline} color="textPrimary"></Typography>
+                    {' — by ' + item.author.name}
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+          ))
+        : null}
     </List>
   );
 }
---
+++ cmd/web/src/store/messages/messages.selectors.ts
@@ -0,0 +1,15 @@
+// Selector are memorized function
+// Helper functions around GET
+// Examples : Having filtered list of author ID
+import { AppState } from '../rootReducer';
+import { createSelector } from '@reduxjs/toolkit';
+import { makeGetAuthor } from '../authors';
+
+export const messagesSelector = (state: AppState) => state.messages;
+// export const getMessages = createSelector(profilesSelector, state => (playlistId: string) => state.byId[playlistId]);
+export const getMessages = createSelector(messagesSelector, state => Object.values(state.byId));
+export const getMessagesWithUser = createSelector(messagesSelector, makeGetAuthor, (state, getAuthor) =>
+  Object.values(state.byId).map(message => {
+    return { message, author: getAuthor(message.authorId) };
+  })
+);
