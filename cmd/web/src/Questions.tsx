3794a323e9b7cb1c2080ad3f0e417d22cd325225
Yann Renaudin <renaudin.yann@gmail.com>
2020-01-03 16:15:58 -0500

    chore: cleanup

--- cmd/web/src/Questions.tsx
+++ cmd/web/src/Questions.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -6,20 +6,9 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-
-// import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from './store/store';
 import { fetchMessages, getMessagesWithUser } from './store/messages';
-
-interface IMessage {
-  id: string;
-  author: string;
-  message: string;
-  likes: number;
-}
-
-const API_URL = 'http://localhost:8080';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -35,21 +24,11 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function AlignItemsList() {
   const classes = useStyles();
-
-  // const [messages, setMessages] = useState<IMessage[]>([]);
   const dispatch = useDispatch<AppDispatch>();
   const messages = useSelector(getMessagesWithUser);
   useEffect(() => {
     dispatch(fetchMessages());
   }, [dispatch]);
-  // useEffect(() => {
-  //   const url = `${API_URL}/messages`;
-  //   axios.get(url).then(response => response.data)
-  //   .then((data) => {
-  //       console.log("Data fetched: ", data)
-  //       setMessages(data)
-  //      })
-  //   }, [])
 
   return (
     <List className={classes.root}>
