096253a2a77d03c095e2375d8741bdf51ce80f8f
Yann Renaudin <renaudin.yann@gmail.com>
2020-01-30 17:40:35 -0500

    feat: finish leaderboard

--- cmd/web/src/Leaderboard.tsx
+++ cmd/web/src/Leaderboard.tsx
@@ -1,28 +1,112 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { getAuthors, getAuthorsWithScore } from './store/authors';
-import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
+import { getAuthorsWithScore } from './store/authors';
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Typography,
+  withStyles,
+  Theme,
+  createStyles,
+  Paper,
+  makeStyles,
+  Avatar,
+  Badge
+} from '@material-ui/core';
+import MedalIcon from 'mdi-react/MedalIcon';
+import { deepOrange, deepPurple, lightGreen, yellow } from '@material-ui/core/colors';
+
+const StyledTableCell = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white
+    },
+    body: {
+      fontSize: 14
+    }
+  })
+)(TableCell);
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    table: {
+      minWidth: 500
+    },
+    rankCell: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    green: {
+      color: theme.palette.getContrastText('#000000'),
+      backgroundColor: '#000000'
+    }
+  })
+);
 
 export const Leaderboard: FC = () => {
+  const classes = useStyles();
   const authorsWithScore = useSelector(getAuthorsWithScore);
+  const sortedAuthorsWithScore = useMemo(
+    () =>
+      authorsWithScore.sort((a, b) => {
+        return b.score - a.score;
+      }),
+    [authorsWithScore]
+  );
+
+  function getMedalColor(index: number) {
+    switch (index) {
+      case 1: {
+        return '#ffdf00';
+      }
+      case 2: {
+        return '#aaa9ad';
+      }
+      case 3: {
+        return '#cd7f32';
+      }
+      default:
+        break;
+    }
+  }
   return (
-    <TableContainer>
-      <Table stickyHeader aria-label="simple table">
+    <TableContainer component={Paper}>
+      <Table className={classes.table} stickyHeader aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Rank</TableCell>
-            <TableCell>ID</TableCell>
-            <TableCell>Score</TableCell>
+            <StyledTableCell>Rank</StyledTableCell>
+            <StyledTableCell>ID</StyledTableCell>
+            <StyledTableCell>Score</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {authorsWithScore.map(({ author, score }, index) => (
+          {sortedAuthorsWithScore.map(({ author, score }, index) => (
             <TableRow hover key={author.id}>
-              <TableCell component="th" scope="row">
-                {index + 1}
+              <TableCell component="th" className={classes.rankCell}>
+                <Badge
+                  overlap="circle"
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right'
+                  }}
+                  badgeContent={index <= 2 ? <MedalIcon color={getMedalColor(index + 1)} /> : null}
+                >
+                  <Avatar className={classes.green}>{index + 1}</Avatar>
+                </Badge>
               </TableCell>
-              <TableCell>{author.id}</TableCell>
-              <TableCell>{score}</TableCell>
+              <TableCell>
+                <Typography>{author.id}</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="body1" color="secondary">
+                  <strong>{score}</strong>
+                </Typography>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
--- cmd/web/src/store/authors/authors.selectors.ts
+++ cmd/web/src/store/authors/authors.selectors.ts
@@ -1,7 +1,8 @@
 import { AppState } from '../rootReducer';
 import { createSelector } from '@reduxjs/toolkit';
 import { getAuthorMessagesById } from '../authorMessages/authorMessages.selectors';
-import { getMessage, messagesSelector } from '../messages/messages.selectors';
+import { getMessage, messagesSelector, getMessages } from '../messages/messages.selectors';
+import { scalePow } from 'd3-scale';
 
 export const authorsSelector = (state: AppState) => state.authors;
 export const makeGetAuthor = createSelector(authorsSelector, state => (authorId: string) => state.byId[authorId]);
@@ -16,19 +17,31 @@ export const getAuthorMessages = createSelector([getAuthorMessagesById, getMessa
   }
 });
 
-export const getAuthorsWithScore = createSelector([authorsSelector, getAuthorMessages], (state, getAuthorMessages) =>
-  Object.values(state.byId).map(author => {
-    const messages = getAuthorMessages(author.id);
-    const countOfMessages = messages.length;
-    const countOfLikes = messages.reduce((acc, message) => {
-      acc += message.likes;
-      return acc;
-    }, 0);
-    return {
-      author,
-      score: (countOfLikes + 1) * countOfMessages
-    };
-  })
+export const getAuthorsWithScore = createSelector(
+  [authorsSelector, getAuthorMessages, getMessages],
+  (state, getAuthorMessages, messages) => {
+    const allLikes = messages.map(message => message.likes);
+    const min = Math.min(...allLikes);
+    const max = Math.max(...allLikes);
+    const scale = scalePow()
+      .exponent(0.45)
+      .domain([min, max])
+      .range([1, 10]);
+    console.log('allLikes', allLikes);
+    return Object.values(state.byId).map(author => {
+      const messages = getAuthorMessages(author.id);
+      const countOfMessages = messages.length;
+      const countOfLikes = messages.reduce((acc, message) => {
+        acc += message.likes;
+        return acc;
+      }, 0);
+      console.log('coeff scaled', scale(countOfLikes));
+      return {
+        author,
+        score: (countOfLikes + 1) * countOfMessages
+      };
+    });
+  }
 );
 
 export const getMessagesWithUser = createSelector(messagesSelector, makeGetAuthor, (state, getAuthor) =>
